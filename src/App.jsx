03b290ff68b7f4832af4f9182f95b5a9c4282f2b
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-10-23 13:52:40 +0200

    add OpenStreetMap API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.css'
+import OpenStreeMap from './components/OpenStreetMap';
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return <div className="App">
+    <OpenStreeMap />
+  </div>;
 }
 
 export default App;
---
+++ src/components/OpenStreetMap.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class OpenStreeMap extends Component {
+
+  render() {
+    return <div className="OpenStreeMap">
+      Voir la carte ici !
+    </div>
+  }
+}
+
+export default OpenStreeMap;
